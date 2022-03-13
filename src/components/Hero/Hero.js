6310ac687dcdc9442f5f0b770b8a1a84ca8b9435
Anurag Chakraborty <chakraborty.anurag01@gmail.com>
2022-03-13 23:58:40 +0530

    added hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Home
+      </SectionTitle>
+      <SectionText>
+        Your portfolio is your resume and your business card. Learn how to build a responsive personal development portfolio from scratch and make your first impression count.
+      </SectionText>
+      <Button onclick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+
 );
 
 export default Hero;
